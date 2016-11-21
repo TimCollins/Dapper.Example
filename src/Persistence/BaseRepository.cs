27d7554771d43d27c4c4a88c572cad29903d433b
Tim <tim@timcollins.ie>
2016-11-21 15:05:10 +0000

    Added BaseRepository.cs to contain database connection. Refactored Category.GetAll() to use Dapper.Query<T>()

---
+++ src/Persistence/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Persistence
+{
+    public class BaseRepository
+    {
+        protected IDbConnection _conn { get; set; }
+
+        public BaseRepository(IDbConnection conn)
+        {
+            _conn = conn;
+            _conn.Open();
+        }
+
+        public BaseRepository()
+        {
+            _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Dapper.Example"].ConnectionString);
+            _conn.Open();
+        }
+    }
+}
--- src/Persistence/CategoryRepository.cs
+++ src/Persistence/CategoryRepository.cs
@@ -1,28 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Data.SqlClient;
+using Dapper;
 using Domain.Interfaces;
 using Domain.Models;
 
 namespace Persistence
 {
-    internal class CategoryRepository : ICategoryRepository
+    internal class CategoryRepository : BaseRepository, ICategoryRepository
     {
         public List<Category> GetAll()
-        {
-            var categories = new List<Category>();
+        {            
             const string sqlQuery = @"SELECT    CategoryID,
                                                 CategoryName,
                                                 Description,
                                                 Picture
                                         FROM Categories";
-
-            using (var reader = DbUtil.ExecuteReader(sqlQuery))
-            {
-                while (reader.Read())
-                {
-                    categories.Add(ReadCategory(reader));
-                }
-            }
+            var categories = _conn.Query<Category>(sqlQuery).AsList();
 
             return categories;
         }

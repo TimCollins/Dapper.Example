e5d5eb60b6115e26cc8796ccbed42a70cf6d26b7
Tim Collins <tim@timcollins.ie>
2021-07-25 18:49:55 +0100

    Create CategoryRepository.cs

---
+++ src/DataAccess/Impl/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Dapper;
+using Domain;
+
+namespace DataAccess.Impl
+{
+    public class CategoryRepository : BaseRepository
+    {
+        public List<Category> GetAll()
+        {
+            var cat = new Domain.Category();
+
+            const string sqlQuery = @"SELECT    CategoryID,
+                                                CategoryName,
+                                                Description,
+                                                Picture
+                                        FROM Categories";
+            var categories = Query<Category>(sqlQuery).AsList();
+
+            return categories;
+        }
+
+    }
+}

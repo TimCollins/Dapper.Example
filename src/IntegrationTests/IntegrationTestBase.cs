87d4ce1c6e02d0e86478db5342d82073bf42ee4f
Tim Collins <tim@timcollins.ie>
2021-07-25 21:12:32 +0100

    Added integration test for category operations.

---
+++ src/IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Impl;
+using DataAccess.Interfaces;
+using SimpleInjector;
+
+namespace IntegrationTests
+{
+    public abstract class IntegrationTestBase
+    {
+        internal readonly Container _container;
+
+        protected IntegrationTestBase()
+        {
+            _container = new Container();
+            _container.Register<ICategoryRepository, CategoryRepository>();
+            _container.Verify();
+        }
+    }
+}

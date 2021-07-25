7a9360fa864d74dd27558a94c740d80e910102b1
Tim Collins <tim@timcollins.ie>
2021-07-25 20:22:25 +0100

    Added the rest of the repository functionality back in.

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DataAccess.Impl;
+using Domain;
 
 namespace ConsoleApp
 {
@@ -8,9 +9,58 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             var categoryRepository = new CategoryRepository();
+
             // Test GetAll()
             var categories = categoryRepository.GetAll();
-            Console.WriteLine("There are {0} categories.", categories.Count);
+            Console.WriteLine($"There are {categories.Count} categories.");
+
+            // Test GetById()
+            var category = categoryRepository.GetById(2);
+            Console.WriteLine($"The second category is \"{category.CategoryName}\".");
+
+            // Test Add()
+            var newCategory = new Category
+            {
+                CategoryName = "Test Category 2",
+                Description = "This is yet another test category"
+            };
+
+            var idToDelete = categoryRepository.Add(newCategory);
+            Console.WriteLine($"Added new category with id of {idToDelete}.");
+
+            // Test Update()
+            var id = 9;
+            category = categoryRepository.GetById(id);
+            category.Description = category.Description + " -- edited";
+            var rowsModified = categoryRepository.Update(category);
+            Console.WriteLine($"Updated existing category. Rows modified: {rowsModified}.");
+
+            // Test Delete<T>()
+            int ret;
+            category = categoryRepository.GetById(idToDelete);
+            if (category == null)
+            {
+                Console.WriteLine($"Category {idToDelete} doesn't exist.");
+            }
+            else
+            {
+                ret = categoryRepository.Delete(category);
+                var categoryCount = categoryRepository.GetAll().Count;
+                Console.WriteLine($"{ret} category was deleted using an object reference. There are now {categoryCount} categories.");
+            }
+
+            // Test Delete(int)
+            id = 11;
+            ret = categoryRepository.Delete(id);
+            if (ret == 0)
+            {
+                Console.WriteLine($"Category {id} doesn't exist.");
+            }
+            else
+            {
+                var categoryCount = categoryRepository.GetAll().Count;
+                Console.WriteLine($"{ret} category was deleted using an id value of {id}. There are now {categoryCount} categories.");
+            }
 
             Util.WaitForEscape();
         }
--- src/DataAccess/Impl/CategoryRepository.cs
+++ src/DataAccess/Impl/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Dapper;
 using Domain;
 
@@ -16,6 +17,60 @@ namespace DataAccess.Impl
             var categories = Query<Category>(sqlQuery).AsList();
 
             return categories;
+        }
+
+        public Category GetById(int id)
+        {
+            const string sqlQuery = @"SELECT	CategoryID,
+                                                CategoryName,
+                                                Description,
+                                                Picture
+                                        FROM Categories
+                                        WHERE CategoryID = @id";
+
+            var category = Query<Category>(sqlQuery, new { id }).FirstOrDefault();
+
+            return category;
+        }
+
+        public int Add(Category category)
+        {
+            const string sqlQuery = @"INSERT Categories(CategoryName, Description) 
+                                        VALUES (@name, @description) 
+                                        SELECT CAST(SCOPE_IDENTITY() AS INT)";
+            var paramsObj = new { name = category.CategoryName, description = category.Description };
+            category.CategoryID = Query<int>(sqlQuery, paramsObj).First();
+
+            return category.CategoryID;
+        }
+
+        public int Update(Category category)
+        {
+            const string sqlQuery = @"UPDATE Categories 
+                                        SET CategoryName = @name,
+                                            Description = @description
+                                        WHERE CategoryID = @id";
+            var paramsObj = new { name = category.CategoryName, description = category.Description, id = category.CategoryID };
+            return Execute(sqlQuery, paramsObj);
+        }
+
+        public int Delete(Category category)
+        {
+            return DeleteById(category.CategoryID);
+        }
+
+        public int Delete(int id)
+        {
+            return DeleteById(id);
+        }
+
+        private int DeleteById(int id)
+        {
+            const string sqlQuery = @"DELETE FROM Categories 
+                                        WHERE CategoryID = @id";
+            var paramsObj = new { id };
+
+            return Execute(sqlQuery, paramsObj);
         }
 
     }

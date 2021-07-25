b5018e9ccb068e0b8e2c65dedf13d932191b76e8
Tim Collins <tim@timcollins.ie>
2021-07-25 18:27:48 +0100

    Read configuration from appsettings.json.

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ConsoleApp
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var config = GetConfig();
             Util.WaitForEscape();
+        }
+
+        private static IConfiguration GetConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(System.AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", true, true);
+
+            return builder.Build();
         }
     }
 }

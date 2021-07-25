8d19bd1feb0d0d07b63cbeb1e7e6ed2181000f08
Tim Collins <tim@timcollins.ie>
2021-07-25 17:24:48 +0100

    Fix type of Id.

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public int CustomerID { get; set; }
+        public string CustomerID { get; set; }
         public string CompanyName { get; set; }
         public string ContactName { get; set; }
         public string ContactTitle { get; set; }
